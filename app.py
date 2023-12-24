7cd39c15c51f19cf7699005c1b77e4e669f07ecd
T <t@delodi.net>
2023-12-24 15:09:03 +0100

    first functioning
    - chromadb running a local db
    - using openai api for now

--- app.py
+++ app.py
@@ -5,27 +5,56 @@ import autogen
 import io
 import json
 import pyairtable
+import chromadb
+import hashlib
+import uuid
 
-from pyairtable import Api
+from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
+from pyairtable import Api
 from bs4 import BeautifulSoup
-from langchain.chat_models import ChatOpenAI
-from dotenv import load_dotenv
-from autogen import config_list_from_json
-from autogen.agentchat.contrib.gpt_assistant_agent import GPTAssistantAgent
-from autogen import UserProxyAgent
 from pypdf import PdfReader
+from langchain.chat_models import ChatOpenAI
 
-load_dotenv()
+
 browserless_api_key = os.getenv("BROWSERLESS_API_KEY")
 serper_api_key = os.getenv("SERP_API_KEY")
 airtable_api_key = os.getenv("AIRTABLE_API_KEY")
-config_list = config_list_from_json("OAI_CONFIG_LIST")
+airtable_base_id = os.getenv("AIRTABLE_BASE_ID")
+airtable_table_rag_id = os.getenv("AIRTABLE_TABLE_RAG_ID")
+airtable_table_question_id = os.getenv("AIRTABLE_TABLE_QUESTION_ID")
+airtable_table_response_id = os.getenv("AIRTABLE_TABLE_RESPONSE_ID")
+airtable_table_final_id = os.getenv("AIRTABLE_TABLE_FINAL_ID")
+
+
+input_dir = "/app/INPUT/"
+chroma_dir = input_dir + "CHROMA"
+
+
+
+# make chroma persistent
+if not os.path.exists(chroma_dir):
+    os.makedirs(chroma_dir)
+
+chromadb_client = chromadb.PersistentClient(path=chroma_dir)
+#chromadb_client = chromadb.Client({'storage': {'path': chroma_dir}})
+
+# these will have to come from a database
+query_text = "How could humans produce materials based on natural processes?"
+query_statement = "nature based materials"
+
+PROMPT_TEMPLATE = """
+Answer the question based on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
 
 # ------------------ Create functions ------------------ #
 
-# Function for google search
 def google_search(search_keyword):    
     url = "https://google.serper.dev/search"
 
@@ -39,11 +68,10 @@ def google_search(search_keyword):
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
-    print("RESPONSE:", response.text)
+    
     return response.text
 
-# Function for scraping
-def summary(objective, content):
+def text_summarize(objective, content):
     llm = ChatOpenAI(temperature = 0, model = "gpt-3.5-turbo-16k-0613")
 
     text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size = 10000, chunk_overlap=500)
@@ -68,11 +96,8 @@ def summary(objective, content):
 
     return output
 
-def web_scraping(objective: str, url: str):
-    #scrape website, and also will summarize the content based on objective if the content is too large
-    #objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
-
-    print("Scraping website...")
+def web_scrape(objective: str, url: str):
+    
     # Define the headers for the request
     headers = {
         'Cache-Control': 'no-cache',
@@ -94,138 +119,161 @@ def web_scraping(objective: str, url: str):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
         text = soup.get_text()
-        print("CONTENTTTTTT:", text)
+        
         if len(text) > 10000:
-            output = summary(objective,text)
+            output = text_summarize(objective,text)
             return output
         else:
             return text
     else:
         print(f"HTTP request failed with status code {response.status_code}")        
 
-
-# Function for get airtable records
-def get_airtable_records(base_id, table_id):
+def airtable_records_get(base_id, table_id):
     api = Api(airtable_api_key)
     table = api.table(base_id, table_id)
     return table.all()
 
-# Function for update airtable records
-
-def write_single_airtable_record(base_id, table_id, fields):
+def airtable_record_write(base_id, table_id, fields):
     api = Api(airtable_api_key)
     table = api.table(base_id, table_id)
     return table.create(fields)
 
-
-# Function for update airtable records
-
-def update_single_airtable_record(base_id, table_id, field_id, fields):
+def airtable_record_update(base_id, table_id, field_id, fields):
     api = Api(airtable_api_key)
     table = api.table(base_id, table_id)
     return table.update(field_id,fields)
 
-# Function to read PDF files from an url
-def pdf_read_from_url(url: str):
-
-    print("#########READING PDF")
-
-
+def pdf_from_url_read(url: str):
     r = requests.get(url)
     f = io.BytesIO(r.content)
-
     reader = PdfReader(f)
     contents = [url]
-
     for page in reader.pages:
         contents.append(page.extract_text().split('\n'))
-
     return contents
 
-# ------------------ Create agent ------------------ #
+def pdf_from_path_read(pdf_path):
+    
+    try:
+        # Open the PDF file
+        with open(pdf_path, 'rb') as file:
+            reader = PdfReader(file)
+            text = []
+            # Read each page from the PDF
+            for page in reader.pages:
+                text.append(page.extract_text())
 
-# Create user proxy agent
-user_proxy = UserProxyAgent(name="user_proxy",
-    is_termination_msg=lambda msg: "TERMINATE" in msg["content"],
-    human_input_mode="NEVER",
-    max_consecutive_auto_reply=1
-    )
+            
+            return ' '.join(text)
+    except Exception as e:
+        print(f"Could not read {pdf_path}: {e}")
+        return None
 
-# Create researcher agent
-researcher = GPTAssistantAgent(
-    name = "researcher",
-    llm_config = {
-        "config_list": config_list,
-        "assistant_id": "asst_GvlOjkiEj1glf1hyLieUzMdT"
-    }
+def scan_directory_for_pdfs(directory):
+    records = []
+    if not os.path.isdir(directory):
+        print(f"The directory {directory} does not exist.")
+        return records
+
+    for filename in os.listdir(directory):
+        if filename.lower().endswith('.pdf'):
+            pdf_path = os.path.join(directory, filename)
+            hash_md5 = hashlib.md5(pdf_path.encode()).hexdigest()
+            records.append({
+                'fields': {
+                    'PDF': pdf_path,
+                    'NAME': hash_md5
+                }
+            })
+    return records
+
+
+#BUILD THE RAGS
+#rag_pdf = airtable_records_get(airtable_base_id,airtable_table_rag_id)
+rag_pdf = scan_directory_for_pdfs(input_dir)
+
+
+# Initialize the text splitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=500,
+    length_function=len,
+    add_start_index=True
 )
-
-researcher.register_function(
-    function_map={
-        "web_scraping": web_scraping,
-        "google_search": google_search,
-        "pdf_read_from_url": pdf_read_from_url,
-        "write_single_airtable_record": write_single_airtable_record
-    }
-)
-
-# Create research manager agent
-research_manager = GPTAssistantAgent(
-    name="research_manager",
-    llm_config = {
-        "config_list": config_list,
-        "assistant_id": "asst_CRNMBrzfmCB7CGGFPHd0Hb9I"
-    }
-)
-
-research_manager.register_function(
-    function_map={
-        "web_scraping": web_scraping,
-        "google_search": google_search,
-        "pdf_read_from_url": pdf_read_from_url,
-        "write_single_airtable_record": write_single_airtable_record
-    }
-)
-
-# Create director agent
-director = GPTAssistantAgent(
-    name = "director",
-    llm_config = {
-        "config_list": config_list,
-        "assistant_id": "asst_weMPr0NNk76xu0m5aydL7zrK",
-    }
-)
-
-director.register_function(
-    function_map={
-        "web_scraping": web_scraping,
-        "google_search": google_search,
-        "pdf_read_from_url": pdf_read_from_url,
-        "write_single_airtable_record": write_single_airtable_record
-    }
-)
+# List to hold collections
+# we might use this if we want the RAGs to critique the final result
+# not for now
+#collections_list = []
 
 
-# Create group chat
-groupchat = autogen.GroupChat(agents=[user_proxy, researcher, research_manager, director], messages=[], max_round=15)
-group_chat_manager = autogen.GroupChatManager(groupchat=groupchat, llm_config={"config_list": config_list})
 
 
-# ------------------ start conversation ------------------ #
-message = """
-We are commissioning a comprehensive study on heliogenesis, a concept proposing that all materials required for human civilization can be produced through natural processes and made freely accessible to everyone globally.
-Do not rely on external databases or institutional access to services, instead rely on searching google and scraping websites.
 
-The first task is to compile an inventory of materials currently utilized by humanity. This inventory will include a list of materials, outlining their key properties, the quantities (in metric tons) in which they are required, and references from existing articles – please ensure all sources are credible and verifiable. The aim is not an exhaustive list of every material in existence, but rather a broad overview capturing the types and quantities of materials essential for our civilization.
-Focus on the materials required for the following fields: construction industry, transport and logistics, fashion, furniture, communication.
-Examples of the materials we intend to find are concrete, clay, copper, steel, plastic, glass, aluminum, wood, bronze, iron, porcelain, rubber, fabric, etc
+# Process each record
+for record in rag_pdf:
+    
+    #if we already have a collection, skip this.
+    if record['fields']['NAME'] not in chromadb_client.list_collections():
+        print(f"reading PDF {record['fields']['PDF']} ")
+        contents = pdf_from_path_read(record['fields']['PDF'])
+        print(f"content read")
+        collection = chromadb_client.create_collection(name=record['fields']['NAME'])
+        # Flatten the list of lists into a single list of strings (lines)
+        all_lines = [line for page in contents[1:] for line in page]
+        # Join the lines to form a single text block
+        full_text = '\n'.join(all_lines)
+        # Split the document into chunks
+        try:
+            print(f"chunking")
+            chunks = text_splitter.split_text(full_text)
+            chunk_ids = []
+            for chunk in chunks:
+                chunk_ids.append(str(uuid.uuid4()))
 
-Focus your research on running Google searches to find PDF files, then read the contents of those PDF files with the provided function pdf_read_from_url.
+            collection.add(
+                documents=chunks,
+                ids=chunk_ids
+                )
+            print(f"collection built")        
+        except AttributeError as e:
+            print(f"An error occurred: {e}")
+    else:
+        print(f"loading collection")        
+        collection = chromadb_client.get_collection(record['fields']['NAME'])
 
-Once the data is gathered, it should be organized and stored in an Airtable. Use the following format for the entries: "MATERIAL", "PROPERTIES", "QUANTITY", "SOURCE". The data should be entered into this specific table: https://airtable.com/appbszeQsW74jcj8l/tblspL8JV5EvyDOfa/viwdw2SsAl3wISS6R?blocks=hide Please stick to these four fields only, without adding any extra information.
 
-This study is a critical step in understanding the scope of materials needed for a heliogenic civilization and will serve as a foundation for further research and development in this area.
-"""
-user_proxy.initiate_chat(group_chat_manager, message=message)
+    # now we get the relevant chunks from the collection
+    results = collection.query(
+        query_texts=[query_statement],
+        n_results=5
+        )
+    print(f"results collected")
+    context_text = ""
+    if 'documents' in results and results['documents']:
+        # Iterate over each document in the results
+        for document_set in results['documents']:
+            for document in document_set:
+                # Append each document's text to the all_documents_string variable
+                context_text += document + "\n\n"  # Add a space between documents for readability
+    else:
+        print("No documents found or 'documents' key not in results.")
+    # then we construct the query
+    prompt_template = PromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    #model = ChatOpenAI()
+    #response_text = model.predict(prompt)
+    #sources = [doc.metadata.get("source", None) for doc, _score in results]
+    #formatted_response = f"Response: {response_text}\nSources: {sources}"
+    #print(formatted_response)
+    # get a resonse from an LLM
+    try:
+        print(f"connecting to LLM")
+        model = ChatOpenAI()
+        response_text = model.predict(prompt)
+        print(response_text)
+        # and finally store the response to airtable
+    except AttributeError as e:
+        print(f"An error occurred: {e}")
 
-#https://en.wikipedia.org/wiki/List_of_companies_of_Germany
+
+
